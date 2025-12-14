91803b572893ea36e915a79847f5f27342c47d42
Huynh Nguyen Tan Phat <huynhnguyentanp@gmail.com>
2025-12-14 22:25:27 +0700

    CRUD nhan vien, phan vai tro(role)

---
+++ src/main/java/com/javaweb/api/admin/UserAPI.java
@@ -0,0 +1,41 @@
+package com.javaweb.api.admin;
+
+import com.javaweb.constant.SystemConstant;
+import com.javaweb.exception.MyException;
+import com.javaweb.model.dto.PasswordDTO;
+import com.javaweb.model.dto.UserDTO;
+import com.javaweb.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserAPI {
+
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDTO> createUsers(@RequestBody UserDTO newUser) {
+        return ResponseEntity.ok(userService.insert(newUser));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUsers(@PathVariable("id") long id, @RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.update(id, userDTO));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteUsers(@RequestBody long[] idList) {
+        if (idList.length > 0) {
+            userService.delete(idList);
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/edit")
+    public void UpdateProfile(@RequestBody UserDTO userDTO) {
+        userService.UpdateInfo(userDTO);
+    }
+}
--- src/main/java/com/javaweb/service/IUserService.java
+++ src/main/java/com/javaweb/service/IUserService.java
@@ -1,8 +1,18 @@
 package com.javaweb.service;
 
 import com.javaweb.model.dto.UserDTO;
+import org.springframework.data.domain.Pageable;
+import java.util.List;
 
 public interface IUserService {
     UserDTO insert(UserDTO userDTO);
     UserDTO findOneByUserNameAndStatus(String name, int status);
+    List<UserDTO> getUsers(String searchValue, Pageable pageable);
+    int countTotalItems();
+    UserDTO findOneByUserName(String userName);
+    UserDTO findUserById(long id);
+    UserDTO update(Long id, UserDTO userDTO);
+    void UpdateInfo(UserDTO userDTO);
+
+    void delete(long[] idList);
 }
