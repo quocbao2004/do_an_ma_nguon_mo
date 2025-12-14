6552ea94e63ef985f09fbd5f5c50f1a788a4eae9
Thuận Phát <90237708+patrickzs@users.noreply.github.com>
2025-12-14 23:26:25 +0700

    CRUD quan ly toa nha

--- src/main/java/com/javaweb/repository/UserRepository.java
+++ src/main/java/com/javaweb/repository/UserRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface UserRepository  extends JpaRepository<UserEntity, Long>, UserRepositoryCustom {
     UserEntity findOneByUserNameAndStatus(String name, int status);
     Page<UserEntity> findByUserNameContainingIgnoreCaseOrFullNameContainingIgnoreCaseAndStatusNot(String userName, String fullName, int status, Pageable pageable);
     Page<UserEntity> findByStatusNot(int status, Pageable pageable);
     UserEntity findOneByUserName(String userName);
+    List<UserEntity> findByStatusAndRoles_Code(Integer status, String roleCode);
 }
